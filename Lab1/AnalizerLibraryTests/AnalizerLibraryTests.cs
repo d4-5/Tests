398cfc02e9a209591bde77b32687b1640634cc3b
d4-5 <111686428+d4-5@users.noreply.github.com>
2023-09-19 21:30:25 +0300

    Lab1

---
+++ Lab1/AnalizerLibraryTests/AnalizerLibraryTests.cs
@@ -0,0 +1,38 @@
+﻿using AnalaizerClassLibrary;
+using ErrorLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AnalaizerClassTests
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"C:\Tests\Lab1\AnalizerLibraryTests\testData.xml", "data", DataAccessMethod.Sequential)]
+        //[DataSource(@"Provider=Microsoft.SqlServerCe.Client.4.0; Data Source=C:\Tests\Lab1\dbTests\dbTests.mdf;", "TestData")]
+        //[DataSource("System.Data.SqlClient", @"Data Source=DESKTOP-E07JQ8M;AttachDbFilename=C:\Tests\Lab1\dbTests\dbTests.mdf;Integrated Security=True;Connect Timeout=5", "TestData", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void Analizer_Format_FromDB()
+        {
+            string expression = Convert.ToString(TestContext.DataRow["expression"]);
+            string error = Convert.ToString(TestContext.DataRow["error"]);
+            int position = Convert.ToInt32(TestContext.DataRow["position"]);
+
+            AnalaizerClass.expression = expression;
+            string result = AnalaizerClass.Format();
+
+            if (position == -1)
+            {
+                Assert.AreEqual("&" + error, result);
+            }
+            else
+            {
+                Assert.AreEqual("&" + ErrorsExpression.GetFullStringError(error, position), result);
+            }
+        }
+    }
+}
+
+
---
+++ Lab1/AnalizerLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41c5e1a0-2771-49ad-b126-cc37bc1e4923")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
